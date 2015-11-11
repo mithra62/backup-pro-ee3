fad9b97562e6bd7368392e44b6d12381032641a2
Eric Lamb <eric@mithra62.com>
2015-11-10 23:48:58 -0800

    Removes Storage Engine icons from dropdown nav

--- system/user/addons/backup_pro/views/storage/_submenu.php
+++ system/user/addons/backup_pro/views/storage/_submenu.php
@@ -7,7 +7,7 @@
 					<div class="sub-menu">
 						<ul>
                              <?php foreach($available_storage_engines AS $section): ?>
-                             <li><a href="<?php echo ee('CP/URL', 'addons/settings/backup_pro/new_storage/'.$section['short_name']); ?>"><img src="<?php echo $theme_folder_url.'backup_pro/images/storage/'.$section['icon']; ?>.png" /> <?php echo $view_helper->m62Lang($section['name']); ?></a></li>
+                             <li><a href="<?php echo ee('CP/URL', 'addons/settings/backup_pro/new_storage/'.$section['short_name']); ?>"><?php echo $view_helper->m62Lang($section['name']); ?></a></li>
                              <?php endforeach; ?>							
 						</ul>
 					</div>
