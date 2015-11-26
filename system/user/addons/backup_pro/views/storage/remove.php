b46c8dc9b4f85086d4c7fc7e92a3f7e5dbfbc966
Eric Lamb <eric@mithra62.com>
2015-11-25 16:51:30 -0800

    Adds box styling

--- system/user/addons/backup_pro/views/storage/remove.php
+++ system/user/addons/backup_pro/views/storage/remove.php
@@ -1,13 +1,29 @@
-<?php $this->load->view('_includes/_errors'); ?>
-<?php $this->load->view('storage/_submenu')?>
-<div class="clear_left shun"></div>
-	
-	<h2><?php echo $view_helper->m62Lang('remove_storage_location'); ?> (<?php echo $storage_details['storage_location_name']; ?>)</h2>
-	
-	<p><strong><?php echo $view_helper->m62Lang($storage_engine['name']); ?>:</strong> <?php echo $view_helper->m62Lang($storage_engine['desc']); ?></p>
-	<?php echo form_open('', array('id'=>'my_accordion'))?>
-
-<fieldset class="form-ctrls">
-		<?php echo form_submit(array('name' => 'submit', 'value' => $view_helper->m62Lang('remove_storage_location'), 'class' => 'btn'));?>
-</fieldset>
-	<?php echo form_close()?>
+<div class="box">
+    <div class="col-group">
+        <div class="col w-16">
+            <?php $this->load->view('_includes/_errors'); ?>
+            	
+            	<h1><?php echo $view_helper->m62Lang('remove_storage_location'); ?> (<?php echo $storage_details['storage_location_name']; ?>)</h1>        	
+                	<?php echo form_open('', array('id'=>'my_accordion', 'class' => 'settings'))?>
+                    <fieldset class="col-group">
+                    	<div class="setting-txt col w-8">
+                            <p><?php echo $view_helper->m62Lang($storage_engine['desc']); ?></p>
+                    	</div>
+                    	<div class="setting-field col w-8 last">
+                    		<?php $this->load->view('storage/_submenu')?>
+                    	</div>
+                    </fieldset>
+                <div class="tbl-ctrls">
+                    <p><?php echo $view_helper->m62Lang('delete_storage_confirm'); ?></p>
+                    
+                    <p class="notice"><?php echo $view_helper->m62Lang('action_can_not_be_undone'); ?></p>   
+                    
+                    <p><?php echo $storage_details['storage_location_name']; ?></p> 
+                    <fieldset class="form-ctrls">
+                    		<?php echo form_submit(array('name' => 'submit', 'value' => $view_helper->m62Lang('remove_storage_location'), 'class' => 'btn'));?>
+                    </fieldset>
+            	<?php echo form_close()?>
+            </div>
+        </div>
+    </div>
+</div>           
