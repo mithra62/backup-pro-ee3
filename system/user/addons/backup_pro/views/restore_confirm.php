469833ae465bc9005fdd81b996bf82a6d89aca96
Eric Lamb <eric@mithra62.com>
2015-12-21 17:00:58 -0800

    Adds table layout to database restore confirm

--- system/user/addons/backup_pro/views/restore_confirm.php
+++ system/user/addons/backup_pro/views/restore_confirm.php
@@ -8,22 +8,33 @@
 <p><?php echo $view_helper->m62Lang('restore_db_question'); ?></p>
 <p class="notice"><?php echo $view_helper->m62Lang('action_can_not_be_undone'); ?> <?php echo $view_helper->m62Lang('restore_double_speak'); ?></p>
 
-<?php echo ($backup['note'] != '' ? '<h2>'.$backup['note'].'</h2>' : ''); ?>
-<p>
-	<strong><?php echo $view_helper->m62Lang('taken'); ?>:</strong> <?php echo $view_helper->m62DateTime($backup['created_date']); ?> <br />
-	<strong><?php echo $view_helper->m62Lang('backup_type'); ?>:</strong> <?php echo $view_helper->m62Lang($backup['database_backup_type']); ?><br />
-	<strong><?php echo $view_helper->m62Lang('verified'); ?>:</strong> 
-		<?php if( $backup['verified'] === 'success'): ?>
-			<span class="success"><?php echo $view_helper->m62Lang('yes'); ?></span>
-		<?php else: ?>
-			<span class="notice"><?php echo $view_helper->m62Lang('no'); ?></span>
-		<?php endif; ?> <br />
-	<strong><?php echo $view_helper->m62Lang('time_taken'); ?>:</strong> <?php echo number_format($backup['time_taken'], 2, '.', ','); ?>s <br />
-	<strong><?php echo $view_helper->m62Lang('max_memory'); ?>:</strong> <?php echo $view_helper->m62FileSize($backup['max_memory']); ?> <br />
-	<strong><?php echo $view_helper->m62Lang('uncompressed_sql_size'); ?>:</strong> <?php echo $view_helper->m62FileSize($backup['uncompressed_size']); ?> <br />
-	<strong><?php echo $view_helper->m62Lang('total_tables'); ?>:</strong> <?php echo $backup['item_count']; ?><br />
-	<strong><?php echo $view_helper->m62Lang('md5_hash'); ?>:</strong> <?php echo $backup['hash']; ?>
-</p>
+<?php 
+$this->table->set_heading('&nbsp;', '');
+if( $backup['note'] != '' )
+{
+    $this->table->add_row(array('<strong>'.$view_helper->m62Lang('note').'</strong>', $backup['note']) );
+}
+$this->table->add_row(array('<strong>'.$view_helper->m62Lang('taken').'</strong>', $view_helper->m62DateTime($backup['created_date'])) );
+$this->table->add_row(array('<strong>'.$view_helper->m62Lang('backup_type').'</strong>', $view_helper->m62Lang($backup['database_backup_type'])));
+
+if( $backup['verified'] === 'success')
+{
+   $verified = '<span class="success">'.$view_helper->m62Lang('yes').'</span>';
+}
+else
+{
+   $verified = '<span class="notice">'.$view_helper->m62Lang('no').'</span>';
+}
+$this->table->add_row(array('<strong>'.$view_helper->m62Lang('verified').'</strong>', $verified));
+$this->table->add_row(array('<strong>'.$view_helper->m62Lang('time_taken').'</strong>', $view_helper->m62TimeFormat($backup['time_taken']) ));
+$this->table->add_row(array('<strong>'.$view_helper->m62Lang('max_memory').'</strong>', $view_helper->m62FileSize($backup['max_memory'])));
+$this->table->add_row(array('<strong>'.$view_helper->m62Lang('uncompressed_sql_size').'</strong>', $view_helper->m62FileSize($backup['uncompressed_size'])));
+$this->table->add_row(array('<strong>'.$view_helper->m62Lang('total_tables').'</strong>', $backup['item_count']));
+$this->table->add_row(array('<strong>'.$view_helper->m62Lang('md5_hash').'</strong>', $backup['hash']));
+echo $this->table->generate();
+$this->table->clear();
+?>
+
 
 		<fieldset class="form-ctrls">
 			<input class="btn submit" type="submit" value="<?php echo $view_helper->m62Lang('restore_db'); ?>">
