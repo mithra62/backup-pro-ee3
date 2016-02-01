45cf2aeb7267c09696e7641e0e34c52278672f08
Eric Lamb <eric@mithra62.com>
2016-01-31 17:49:49 -0800

    Adds reduced storage and optional folder path

--- system/user/addons/backup_pro/views/storage/drivers/_gcs.php
+++ system/user/addons/backup_pro/views/storage/drivers/_gcs.php
@@ -1,3 +1,4 @@
+<input type="hidden" value="0" name="gcs_reduced_redundancy" />
 <fieldset class="col-group <?php echo (is_array($form_errors['gcs_access_key']) ? 'invalid' : 'security-enhance'); ?>">
 	<div class="setting-txt col w-8">
 		<h3><label for="gcs_access_key"><?php echo $view_helper->m62Lang('gcs_access_key'); ?></label></h3>
@@ -28,5 +29,29 @@
 	<div class="setting-field col w-8">
 		<?php echo form_input('gcs_bucket', $form_data['gcs_bucket'], 'id="gcs_bucket"'); ?>
 		<?php echo $view_helper->m62FormErrors($form_errors['gcs_bucket']); ?>
+	</div>
+</fieldset>
+
+<fieldset class="col-group <?php echo (is_array($form_errors['gcs_optional_prefix']) ? 'invalid' : ''); ?>">
+	<div class="setting-txt col w-8">
+		<h3><label for="gcs_optional_prefix"><?php echo $view_helper->m62Lang('gcs_optional_prefix'); ?></label></h3>
+		<em><?php echo $view_helper->m62Lang('gcs_optional_prefix_instructions'); ?></em>
+	</div>
+	<div class="setting-field col w-8">
+		<?php echo form_input('gcs_optional_prefix', $form_data['gcs_optional_prefix'], 'id="gcs_optional_prefix"'); ?>
+		<?php echo $view_helper->m62FormErrors($form_errors['gcs_optional_prefix']); ?>
+	</div>
+</fieldset>
+
+<fieldset class="col-group <?php echo (is_array($form_errors['gcs_reduced_redundancy']) ? 'invalid' : ''); ?>">
+	<div class="setting-txt col w-8">
+		<h3><label for="gcs_reduced_redundancy"><?php echo $view_helper->m62Lang('gcs_reduced_redundancy'); ?></label></h3>
+		<em><?php echo $view_helper->m62Lang('gcs_reduced_redundancy_instructions'); ?></em>
+	</div>
+	<div class="setting-field col w-8">
+		<label class="choice mr <?php echo ($form_data['gcs_reduced_redundancy'] == '1' ? 'chosen' : ''); ?>">
+		  <?php echo form_checkbox('gcs_reduced_redundancy', '1', $form_data['gcs_reduced_redundancy'], 'id="gcs_reduced_redundancy"'); ?>
+		</label>
+		<?php echo $view_helper->m62FormErrors($form_errors['gcs_reduced_redundancy']); ?>
 	</div>
 </fieldset>
